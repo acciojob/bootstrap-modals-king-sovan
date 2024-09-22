b9a067353ea56d82adbb99f25d293076ea9c74a1
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-22 13:06:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your code here
+document.addEventListener('DOMContentLoaded', function() {
+	var openModalBtn = document.getElementById('open-modal');
+	var modal = new bootstrap.Modal(document.getElementById('newton-modal'), {
+		keyboard: false
+	});
+
+	openModalBtn.addEventListener('click', function() {
+		modal.show();
+	});
+});
